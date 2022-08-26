70c4f1f4df22ceeeae5cce6f9d5f501a72a2033b
Cesar Gonzalez <cegonse@gmail.com>
2022-08-26 17:11:23 +0200

    Render game as fixed size render texture

--- src/engine/pc/window.c
+++ src/engine/pc/window.c
@@ -8,6 +8,12 @@
 #include <engine/resource_pack.h>
 #include <engine/window.h>
 
+static RenderTexture2D render_texture;
+
+void Drawing_Init(int viewport_width, int viewport_height) {
+
+}
+
 static void logCallback(int logLevel, const char *text, va_list args)
 {
   char log[4096];
@@ -20,20 +26,40 @@ void Window_Open(struct window_settings settings)
 {
   SetTraceLogCallback(logCallback);
 
-  InitWindow(settings.width, settings.width, settings.title);
-  SetTargetFPS(60);
+  InitWindow(settings.width, settings.height, settings.title);
+  SetWindowState(FLAG_WINDOW_RESIZABLE);
+  SetTargetFPS(
+    GetMonitorRefreshRate(GetCurrentMonitor())
+  );
 
   TextureRepository_Init();
   ResourcePack_Init();
 
+  render_texture = LoadRenderTexture(settings.width, settings.height);
+
+  BeginTextureMode(render_texture);
+  ClearBackground(RAYWHITE);
+  EndTextureMode();
+
   ResourcePack_Load("default");
 
-  while (!WindowShouldClose())
-  {
+  while (!WindowShouldClose()) {
+    BeginTextureMode(render_texture);
     settings.onDraw();
+    EndTextureMode();
+
+    BeginDrawing();
+    DrawTextureRec(
+      render_texture.texture,
+      (Rectangle) { 0, 0, (float)render_texture.texture.width, (float)-render_texture.texture.height },
+      (Vector2) { 0, 0 },
+      WHITE
+    );
+    EndDrawing();
   }
 
   settings.onExit();
+
   ResourcePack_Destroy();
   TextureRepository_Destroy();
   CloseWindow();
